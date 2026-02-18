09d0fad732dddb8ebf7a3b05667b404156af4637
Sy Soto <s.soto@ymail.com>
2026-02-17 20:34:50 -0500

    Updated several code files based on feedback and ruberic requirements.

--- app.js
+++ app.js
@@ -31,43 +31,142 @@ const { engine } = require('express-handlebars');
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+
 // ########################################
 // ########## ROUTES
+
 
 // HOME
 app.get('/', async function (req, res) {
     res.render('home');
 });
 
+
+
 // STUDENTS
+
+// Browse Students
 app.get('/students', async function (req, res) {
     const [students] = await db.query('SELECT * FROM Students;');
     res.render('students', { students: students });
 });
 
+// Add Student
+app.post('/students/add', async function (req, res) {
+    const { firstName, lastName, email, enrollmentStatus } = req.body;
+
+    await db.query(
+        'INSERT INTO Students (firstName, lastName, email, enrollmentStatus) VALUES (?, ?, ?, ?);',
+        [firstName, lastName, email, enrollmentStatus]
+    );
+
+    res.redirect('/students');
+});
+
+// Delete Student
+app.post('/students/delete', async function (req, res) {
+    const { studentID } = req.body;
+
+    await db.query(
+        'DELETE FROM Students WHERE studentID = ?;',
+        [studentID]
+    );
+
+    res.redirect('/students');
+});
+
+// Update Student
+app.post('/students/update', async function (req, res) {
+    const { studentID, email, enrollmentStatus } = req.body;
+
+    await db.query(
+        'UPDATE Students SET email = ?, enrollmentStatus = ? WHERE studentID = ?;',
+        [email, enrollmentStatus, studentID]
+    );
+
+    res.redirect('/students');
+});
+
+
+
 // COURSES
+
 app.get('/courses', async function (req, res) {
     const [courses] = await db.query('SELECT * FROM Courses;');
     res.render('courses', { courses: courses });
 });
 
+
+
 // INSTRUCTORS
+
 app.get('/instructors', async function (req, res) {
     const [instructors] = await db.query('SELECT * FROM Instructors;');
     res.render('instructors', { instructors: instructors });
 });
 
+
+
 // SECTIONS
+
 app.get('/sections', async function (req, res) {
     const [sections] = await db.query('SELECT * FROM Sections;');
     res.render('sections', { sections: sections });
 });
 
-// ENROLLMENTS
+
+
+// ENROLLMENTS (M:N)
+
+// Browse Enrollments with JOIN for readable student name
 app.get('/enrollments', async function (req, res) {
-    const [enrollments] = await db.query('SELECT * FROM Enrollments;');
-    res.render('enrollments', { enrollments: enrollments });
+
+    const [enrollments] = await db.query(`
+        SELECT 
+            Enrollments.enrollmentID,
+            CONCAT(Students.firstName, ' ', Students.lastName) AS studentName,
+            Enrollments.sectionID,
+            Enrollments.enrollmentDate,
+            Enrollments.enrollmentStatus
+        FROM Enrollments
+        JOIN Students ON Enrollments.studentID = Students.studentID
+        JOIN Sections ON Enrollments.sectionID = Sections.sectionID;
+    `);
+
+    const [students] = await db.query('SELECT * FROM Students;');
+    const [sections] = await db.query('SELECT * FROM Sections;');
+
+    res.render('enrollments', {
+        enrollments: enrollments,
+        students: students,
+        sections: sections
+    });
 });
+
+// Add Enrollment (M:N INSERT)
+app.post('/enrollments/add', async function (req, res) {
+    const { studentID, sectionID, enrollmentDate, enrollmentStatus } = req.body;
+
+    await db.query(
+        'INSERT INTO Enrollments (studentID, sectionID, enrollmentDate, enrollmentStatus) VALUES (?, ?, ?, ?);',
+        [studentID, sectionID, enrollmentDate, enrollmentStatus]
+    );
+
+    res.redirect('/enrollments');
+});
+
+// Update Enrollment (M:N UPDATE)
+app.post('/enrollments/update', async function (req, res) {
+    const { enrollmentID, sectionID } = req.body;
+
+    await db.query(
+        'UPDATE Enrollments SET sectionID = ? WHERE enrollmentID = ?;',
+        [sectionID, enrollmentID]
+    );
+
+    res.redirect('/enrollments');
+});
+
 
 // ########################################
 // ########## LISTENER
